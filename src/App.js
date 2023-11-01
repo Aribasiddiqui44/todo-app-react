40b1d4698a4c4d0dc7d706ee3c73f39561746f32
Ariba Siddiqui <aribasiddi4@gmail.com>
2023-11-01 11:32:07 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import "./App.css"
 import TodoInput from './components/TodoInput'
 import Todolist from './components/TodoList';
+
+const getLocalItems = () => {
+  let list = localStorage.getItem('Todoitems');
+  console.log(list);
+
+  if(list) {
+    return JSON.parse(localStorage.getItem('Todoitems'));
+  }
+  else{
+    return [];
+  }
+}
+
 function App() {
-  const [listTodo,setListTodo]=useState([]);
+  const [listTodo,setListTodo]=useState(getLocalItems());
   let addList = (inputText)=>{
     if(inputText!=='')
       setListTodo([...listTodo,inputText]);
@@ -13,6 +26,11 @@ function App() {
     newListTodo.splice(key,1)
     setListTodo([...newListTodo])
   }
+
+  useEffect(()=>{
+    localStorage.setItem('Todoitems', JSON.stringify(listTodo))
+  },[listTodo]);
+
   return (
     <div className="main-container">
       <div className="center-container">
